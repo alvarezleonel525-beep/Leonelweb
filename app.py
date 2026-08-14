15b35cfb7bc3f39a475d5d37e7c8e53e187232fe
Leonel <alvarezleonel525@gmail.com>
2026-08-14 15:09:51 -0300

    Agregar callback de Kick

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def inicio():
     return render_template("index.html")
 
+@app.route("/callback")
+def callback():
+    return "Autorización recibida correctamente. Podés cerrar esta página."
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
